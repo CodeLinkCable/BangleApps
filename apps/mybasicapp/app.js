45f97d3704f7c4f96a80e08eecd801a08f65f153
Andrew <42917293+CodeLinkCable@users.noreply.github.com>
2025-08-28 21:38:47 -0700

    Update app.js

--- apps/mybasicapp/app.js
+++ apps/mybasicapp/app.js
@@ -1,1 +1,12 @@
+// Clear screen
+g.clear();
 
+// Centered text
+g.setFont("6x8", 2);
+g.setFontAlign(0, 0);
+g.drawString("Hello Bangle!", g.getWidth()/2, g.getHeight()/2);
+
+// Exit to launcher on BTN1
+setWatch(() => {
+  load();
+}, BTN1);
