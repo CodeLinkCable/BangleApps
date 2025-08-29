743125469fe1dd047bca3a303b91505e4c0138a5
Andrew <42917293+CodeLinkCable@users.noreply.github.com>
2025-08-28 21:06:10 -0700

    Update app.js

--- apps/pokewatch/app.js
+++ apps/pokewatch/app.js
@@ -1,1 +1,8 @@
-E.showMessage("My\nSimple\nApp","My App")
+g.clear();
+g.setFont("6x8",2);
+g.setFontAlign(0,0); // center
+g.drawString("Hello Bangle!", g.getWidth()/2, g.getHeight()/2);
+
+setWatch(() => {
+  load(); // go back to launcher when button is pressed
+}, BTN1);
